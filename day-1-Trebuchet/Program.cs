037894af357c82b722c6ffe2e93d8cd8ae98b772
Andrew Kehrig <andrew.kehrig@controlant.com>
2023-12-01 18:39:19 +0000

    Part 1 Completion

--- day-1-Trebuchet/Program.cs
+++ day-1-Trebuchet/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+// get input file from user input
+Console.WriteLine("Enter the path to the input file:");
+var path = Console.ReadLine();
+var lines = File.ReadLines(path ?? throw new InvalidOperationException());
+var sum = lines.Select(line => Regex.Matches(line, @"\d")
+        .Select(m => m.Value)
+        .ToArray())
+    .Aggregate(0, (current, digits) => current + int.Parse($"{digits[0]}{digits[^1]}"));
+
+Console.WriteLine($"The sum of all the concatenated digit lines is: {sum}");
